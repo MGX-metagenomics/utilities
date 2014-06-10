7ac59b23238b9d010ee959ed5319d56d417b0a30
Sebastian Jaenicke <sebastian@jaenicke.org>
2014-06-10 08:41:18 +0200

    replace equals/hashCode implementations

--- MGX-API/src/main/java/de/cebitec/mgx/api/misc/Triple.java
+++ MGX-API/src/main/java/de/cebitec/mgx/api/misc/Triple.java
@@ -1,5 +1,7 @@
 package de.cebitec.mgx.api.misc;
 
+import java.util.Objects;
+
 /**
  *
  * @author sjaenick
@@ -27,4 +29,36 @@ public class Triple<T, U, V> {
     public V getThird() {
         return third;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.first);
+        hash = 29 * hash + Objects.hashCode(this.second);
+        hash = 29 * hash + Objects.hashCode(this.third);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Triple<?, ?, ?> other = (Triple<?, ?, ?>) obj;
+        if (!Objects.equals(this.first, other.first)) {
+            return false;
+        }
+        if (!Objects.equals(this.second, other.second)) {
+            return false;
+        }
+        if (!Objects.equals(this.third, other.third)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 }
