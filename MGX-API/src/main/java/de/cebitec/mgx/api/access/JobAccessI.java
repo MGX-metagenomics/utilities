c462637dab39f41f8b09391fb95f5c38874df5ba
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2014-07-24 17:28:37 +0200

    minor API update, adapt implementation

--- MGX-API/src/main/java/de/cebitec/mgx/api/access/JobAccessI.java
+++ MGX-API/src/main/java/de/cebitec/mgx/api/access/JobAccessI.java
@@ -8,14 +8,28 @@ package de.cebitec.mgx.api.access;
 import de.cebitec.mgx.api.exception.MGXException;
 import de.cebitec.mgx.api.misc.TaskI;
 import de.cebitec.mgx.api.model.JobI;
+import de.cebitec.mgx.api.model.JobParameterI;
 import de.cebitec.mgx.api.model.SeqRunI;
+import de.cebitec.mgx.api.model.ToolI;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 /**
  *
  * @author sj
  */
-public interface JobAccessI extends AccessBaseI<JobI> {
+public interface JobAccessI  {
+
+    public JobI create(ToolI tool, SeqRunI seqrun, Collection<JobParameterI> params);
+
+    public JobI fetch(long id);
+
+    public Iterator<JobI> fetchall();
+
+    public void update(JobI obj);
+
+    public TaskI delete(JobI obj);
 
     public List<JobI> BySeqRun(SeqRunI run);
 
