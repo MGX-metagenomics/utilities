b2177ddae853796b9da9ed5838aca056ab52affe
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2015-01-15 16:02:49 +0100

    remove dependency on ejb project, copy relevant classes

---
+++ MGX-admin/src/main/java/de/cebitec/mgx/admin/misc/Region.java
@@ -0,0 +1,74 @@
+package de.cebitec.mgx.admin.misc;
+
+/**
+ *
+ * @author belmann
+ */
+import java.io.Serializable;
+
+/**
+ *
+ * @author sjaenick
+ */
+public class Region implements Serializable {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Reference reference;
+    private int start;
+    private int stop;
+
+    public Reference getReference() {
+        return reference;
+    }
+
+    public void setReference(Reference reference) {
+        this.reference = reference;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getStop() {
+        return stop;
+    }
+
+    public void setStop(int stop) {
+        this.stop = stop;
+    }
+
+    @Override
+    public String toString() {
+        return "de.cebitec.mgx.model.db.Region";
+    }
+}
