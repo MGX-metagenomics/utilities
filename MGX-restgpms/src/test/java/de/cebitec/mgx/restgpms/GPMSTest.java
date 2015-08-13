c2cb075d61f3426650bcde02988701bceff5ef12
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2015-08-13 11:50:54 +0200

    protobuf update

--- MGX-restgpms/src/test/java/de/cebitec/mgx/restgpms/GPMSTest.java
+++ MGX-restgpms/src/test/java/de/cebitec/mgx/restgpms/GPMSTest.java
@@ -34,7 +34,7 @@ public class GPMSTest {
                 junitBundles(),
                 mavenBundle().groupId("com.sun.jersey").artifactId("jersey-client").version("1.18.2"),
                 mavenBundle().groupId("com.sun.jersey").artifactId("jersey-core").version("1.18.2"),
-                mavenBundle().groupId("com.google.protobuf").artifactId("protobuf-java").version("2.5.0"),
+                mavenBundle().groupId("com.google.protobuf").artifactId("protobuf-java").version("2.6.1"),
                 mavenBundle().groupId("de.cebitec.gpms").artifactId("GPMS-DTO").version("1.1"),
                 mavenBundle().groupId("de.cebitec.gpms").artifactId("GPMS-core-api").version("1.1"),
                 mavenBundle().groupId("de.cebitec.gpms").artifactId("GPMS-rest-api").version("1.1"),
