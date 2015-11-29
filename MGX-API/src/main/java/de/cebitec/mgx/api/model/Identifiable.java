2e9356cbc9ef7ee7a86d209fc79b2aa845dd2401
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2015-11-29 13:34:36 +0100

    api refactoring, add preliminary replicate support

--- MGX-API/src/main/java/de/cebitec/mgx/api/model/Identifiable.java
+++ MGX-API/src/main/java/de/cebitec/mgx/api/model/Identifiable.java
@@ -7,7 +7,7 @@ import java.awt.datatransfer.DataFlavor;
  *
  * @author sj
  */
-public abstract class Identifiable<T> extends ModelBase<T> {
+public abstract class Identifiable<T extends MGXDataModelBase<T>> extends MGXDataModelBase<T> {
 
     public final static long INVALID_IDENTIFIER = -1;
     protected long id = INVALID_IDENTIFIER;
