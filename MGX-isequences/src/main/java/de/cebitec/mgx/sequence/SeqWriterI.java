f3cf3b8b71e5f3cd5e1e51949735f384a4cbe6e1
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2016-04-05 15:29:14 +0200

    handle changed exception type

--- MGX-isequences/src/main/java/de/cebitec/mgx/sequence/SeqWriterI.java
+++ MGX-isequences/src/main/java/de/cebitec/mgx/sequence/SeqWriterI.java
@@ -1,13 +1,11 @@
 package de.cebitec.mgx.sequence;
 
-import java.io.IOException;
-
 /**
  *
  * @author sjaenick
  */
 public interface SeqWriterI<T extends DNASequenceI> extends AutoCloseable {
     
-    public void addSequence(T seq) throws IOException;
+    public void addSequence(T seq) throws SeqStoreException;
 
 }
