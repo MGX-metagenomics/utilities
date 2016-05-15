c1a2ee75710a9da499adacf0d6c4caea12e71205
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2016-05-15 17:47:27 +0200

    avoid array allocation

--- MGX-seqstorage/src/main/java/de/cebitec/mgx/seqstorage/NMSReader.java
+++ MGX-seqstorage/src/main/java/de/cebitec/mgx/seqstorage/NMSReader.java
@@ -15,49 +15,48 @@ import java.util.logging.Logger;
  */
 public class NMSReader {
 
-        final TLongLongMap idx;
-        final InputStream nmsStream;
+    private final TLongLongMap idx;
+    private final InputStream nmsStream;
+    private final byte[] buf = new byte[16];
 
-        public NMSReader(InputStream nmsStream) throws IOException {
-            this.idx = new TLongLongHashMap(100, 1.0F, -1, -1);
-            this.nmsStream = nmsStream;
-        }
+    public NMSReader(InputStream nmsStream) throws IOException {
+        this.idx = new TLongLongHashMap(100, 1.0F, -1, -1);
+        this.nmsStream = nmsStream;
+    }
 
-        public long getOffset(long id) throws SeqStoreException {
-            if (!idx.containsKey(id)) {
-                try {
-                    readRequired(id);
-                } catch (IOException ex) {
-                    throw new SeqStoreException(ex.getMessage());
-                }
-            }
-            return idx.get(id);
-        }
-
-        private void readRequired(long id) throws IOException {
-            byte[] buf = new byte[16];
-            while (16 == nmsStream.read(buf)) {
-                long curId = ByteUtils.bytesToLong(ByteUtils.substring(buf, 0, 7));
-                long offset = ByteUtils.bytesToLong(ByteUtils.substring(buf, 8, 15));
-                idx.put(curId, offset);
-
-                if (curId == id) {
-                    return;
-                }
-            }
-        }
-
-        public void close() {
+    public long getOffset(long id) throws SeqStoreException {
+        if (!idx.containsKey(id)) {
             try {
-                if (nmsStream != null) {
-                    nmsStream.close();
-                }
-                if (idx != null) {
-                    idx.clear();
-                }
+                readRequired(id);
             } catch (IOException ex) {
-                Logger.getLogger(CSFReader.class.getName()).log(Level.SEVERE, null, ex);
+                throw new SeqStoreException(ex.getMessage());
+            }
+        }
+        return idx.get(id);
+    }
+
+    private void readRequired(long id) throws IOException {
+        while (16 == nmsStream.read(buf)) {
+            long curId = ByteUtils.bytesToLong(buf, 0);
+            long offset = ByteUtils.bytesToLong(buf, 8);
+            idx.put(curId, offset);
+
+            if (curId == id) {
+                return;
             }
         }
     }
 
+    public void close() {
+        try {
+            if (nmsStream != null) {
+                nmsStream.close();
+            }
+            if (idx != null) {
+                idx.clear();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(CSFReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
