c3342e5fed19fdb66435fc70fd62b4a692da7ea9
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2016-05-29 11:21:13 +0200

    close statements

--- KEGG-Pathways/src/main/java/de/cebitec/mgx/kegg/pathways/KEGGMaster.java
+++ KEGG-Pathways/src/main/java/de/cebitec/mgx/kegg/pathways/KEGGMaster.java
@@ -14,6 +14,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -141,7 +142,9 @@ public class KEGGMaster implements AutoCloseable {
                             + "   mapnum VARCHAR(10), "
                             + "   name VARCHAR(512), "
                             + "   UNIQUE(mapnum))";
-                    conn.createStatement().execute(sql);
+                    try (Statement stmt = conn.createStatement()) {
+                        stmt.execute(sql);
+                    }
 
 //                sql = "CREATE TABLE IF NOT EXISTS ecnumber ("
 //                        + "   number TEXT, "
@@ -158,12 +161,16 @@ public class KEGGMaster implements AutoCloseable {
                             //                       + "   FOREIGN KEY(ec_num) REFERENCES ecnumber(number),"
                             + "   UNIQUE(pw_num, ec_num, x, y) "
                             + ")";
-                    conn.createStatement().execute(sql);
+                    try (Statement stmt = conn.createStatement()) {
+                        stmt.execute(sql);
+                    }
 
                     sql = "CREATE TABLE IF NOT EXISTS timestamps ("
                             + "type VARCHAR(30), "
                             + "time DATETIME)";
-                    conn.createStatement().execute(sql);
+                    try (Statement stmt = conn.createStatement()) {
+                        stmt.execute(sql);
+                    }
                 }
             }
         } catch (SQLException ex) {
