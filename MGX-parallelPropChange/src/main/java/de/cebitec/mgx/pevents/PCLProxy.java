62e80bde12f0041dcabf0f095627604641f7cdf8
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2016-10-06 10:49:54 +0200

    get rid of pclproxy

---
+++ MGX-parallelPropChange/src/main/java/de/cebitec/mgx/pevents/PCLProxy.java
@@ -0,0 +1,30 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package de.cebitec.mgx.pevents;
+//
+//import java.beans.PropertyChangeEvent;
+//import java.beans.PropertyChangeListener;
+//
+///**
+// *
+// * @author sj
+// */
+//public class PCLProxy implements PropertyChangeListener {
+//
+//    private final PropertyChangeListener target;
+//    public PCLProxy(PropertyChangeListener listener) {
+//        this.target = listener;
+//    }
+//
+//    @Override
+//    public void propertyChange(PropertyChangeEvent evt) {
+//        target.propertyChange(evt);
+//    }
+//
+//    public PropertyChangeListener getTarget() {
+//        return target;
+//    }
+//}
