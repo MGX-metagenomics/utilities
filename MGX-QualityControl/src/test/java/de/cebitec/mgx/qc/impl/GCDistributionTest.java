c455de39fcb2381138b0173ea06b2278fa396799
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2016-10-11 13:05:38 +0200

    improve tests

--- MGX-QualityControl/src/test/java/de/cebitec/mgx/qc/impl/GCDistributionTest.java
+++ MGX-QualityControl/src/test/java/de/cebitec/mgx/qc/impl/GCDistributionTest.java
@@ -66,8 +66,7 @@ public class GCDistributionTest {
     @Test
     public void testAdd() throws SeqStoreException {
         System.out.println("add");
-        Assume.assumeTrue(new File("/homes/sjaenick/testdata.fas").exists());
-        FastaReader r = new FastaReader("/homes/sjaenick/testdata.fas", false);
+        FastaReader r = new FastaReader("src/test/resources/de/cebitec/mgx/qc/testdata.fas", false);
         GCDistribution gc = new GCDistribution();
         while (r.hasMoreElements()) {
             gc.add(r.nextElement());
