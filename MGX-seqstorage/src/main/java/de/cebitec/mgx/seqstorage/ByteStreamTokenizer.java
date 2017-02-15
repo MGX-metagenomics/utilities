df3c3f490053d3d41910cdc4efe8d13a629e2cb7
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2017-02-15 13:55:37 +0100

    fix int overflow

--- MGX-seqstorage/src/main/java/de/cebitec/mgx/seqstorage/ByteStreamTokenizer.java
+++ MGX-seqstorage/src/main/java/de/cebitec/mgx/seqstorage/ByteStreamTokenizer.java
@@ -2,7 +2,6 @@ package de.cebitec.mgx.seqstorage;
 
 import de.cebitec.mgx.seqstorage.encoding.ByteUtils;
 import de.cebitec.mgx.sequence.SeqStoreException;
-import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -30,32 +29,30 @@ public class ByteStreamTokenizer implements Iterator<byte[]> {
 
         // set up reader, skip over offset bytes, pre-fill buffer
         if (gzipCompressed) {
-            InputStream gzstream = new GZIPInputStream(new FileInputStream(fname));
-            in = new BufferedInputStream(gzstream);
+            in = new GZIPInputStream(new FileInputStream(fname), 49152);
         } else {
-            in = new BufferedInputStream(new FileInputStream(fname));
+            in = new FileInputStream(fname);
         }
         long skipped = in.skip(skipBytes);
         if (skipped != skipBytes) {
-            throw new SeqStoreException("Could not skip "+skipBytes+ " bytes.");
+            throw new SeqStoreException("Could not skip " + skipBytes + " bytes.");
         }
         fillBuffer();
     }
 
     @Override
     public boolean hasNext() {
-        
+
         if (elem != null) {
             // previous element not yet received
             return true;
         }
-        
+
         int breakpos;
 
         /* 
          * try to serve request from buffer
          */
-
         if ((breakpos = getSeparatorPos(startpos, endpos)) != -1) {
             elem = ByteUtils.substring(buffer, startpos, breakpos - 1);
 
@@ -75,11 +72,15 @@ public class ByteStreamTokenizer implements Iterator<byte[]> {
          * buffer unable to handle request, need to fetch more data from file,
          * if possible
          */
-
         while (!atEOF && (breakpos = getSeparatorPos(startpos, endpos)) == -1) {
             // move buffer contents to front of buffer
             moveBuffer(startpos);
-            enlargeBuffer();
+
+            // do not double the buffer size unless we have reached 90%
+            // buffer usage
+            if (endpos > bufferRefillPosition) {
+                enlargeBuffer();
+            }
 
             if (fillBuffer()) {
                 break;
@@ -91,7 +92,6 @@ public class ByteStreamTokenizer implements Iterator<byte[]> {
          * there aren't any, unless the final record isn't terminated with the 
          * separator char
          */
-
         if ((breakpos = getSeparatorPos(startpos, endpos)) != -1) {
             elem = ByteUtils.substring(buffer, startpos, breakpos - 1);
             startpos = breakpos + 1;
@@ -99,8 +99,8 @@ public class ByteStreamTokenizer implements Iterator<byte[]> {
         } else {
             if (atEOF && startpos <= endpos) {
                 // at EOF, data present but not terminated with separator
-                elem = ByteUtils.substring(buffer, startpos, endpos-1);
-                startpos = endpos+1;
+                elem = ByteUtils.substring(buffer, startpos, endpos - 1);
+                startpos = endpos + 1;
                 return true;
             }
             return false;
@@ -151,19 +151,20 @@ public class ByteStreamTokenizer implements Iterator<byte[]> {
             if ((bytesRead = in.read(buffer, endpos, buffer.length - endpos)) != -1) {
                 //System.err.println("read " + bytesRead + " bytes");
                 endpos += bytesRead;
+//                System.err.println("filled buffer with additional "+bytesRead+" up to " + endpos + ", fpos " + filePos);
             } else {
                 atEOF = true;
                 return true;
             }
         } catch (IOException ex) {
-            //System.err.println("EXCEPTION");
+            System.err.println("EXCEPTION");
         }
         return false;
     }
 
     private void enlargeBuffer() {
         // double the buffer size and copy contents
-        //System.err.println("buffer doubled to " + buffer.length * 2 + ", startpos " + startpos + ", endpos " + endpos);
+//        System.err.println("buffer doubling to " + buffer.length * 2 + ", startpos " + startpos + ", endpos " + endpos);
         byte newbuf[] = new byte[buffer.length * 2];
         System.arraycopy(buffer, 0, newbuf, 0, buffer.length);
         buffer = newbuf;
