b30939e26e477cf43c9da8279b2398a9ad8e1fb0
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2020-04-24 16:58:16 +0200

    thread-safety fixes

--- MGX-parallelPropChange/src/main/java/de/cebitec/mgx/pevents/Activator.java
+++ MGX-parallelPropChange/src/main/java/de/cebitec/mgx/pevents/Activator.java
@@ -5,6 +5,7 @@
  */
 package de.cebitec.mgx.pevents;
 
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.osgi.framework.BundleActivator;
@@ -16,8 +17,8 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator implements BundleActivator {
 
-    private static long numDirect = 0;
-    private static long numThreaded = 0;
+    private static final AtomicLong numDirect = new AtomicLong(0);
+    private static final AtomicLong numThreaded = new AtomicLong(0);
     private final static boolean DUMP_STATS = true;
 
     @Override
@@ -27,15 +28,17 @@ public class Activator implements BundleActivator {
     @Override
     public void stop(BundleContext context) throws Exception {
         if (DUMP_STATS) {
-            Logger.getLogger(getClass().getPackage().getName()).log(Level.INFO, "Terminating with {0} direct and {1} async event deliveries.", new Object[]{numDirect, numThreaded});
+            Logger.getLogger(getClass().getPackage().getName())
+                    .log(Level.INFO, "Terminating with {0} direct and {1} async event deliveries.",
+                            new Object[]{numDirect.get(), numThreaded.get()});
         }
     }
 
     public static synchronized void direct(int num) {
-        numDirect += num;
+        numDirect.addAndGet(num);
     }
 
     public static synchronized void async(int num) {
-        numThreaded += num;
+        numThreaded.addAndGet(num);
     }
 }
