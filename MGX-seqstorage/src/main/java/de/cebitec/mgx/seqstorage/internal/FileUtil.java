8d4ed214950e7b851ba85a5f7469f57b99edc1dd
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2020-08-25 12:37:31 +0200

    minor restructuring

--- MGX-seqstorage/src/main/java/de/cebitec/mgx/seqstorage/internal/FileUtil.java
+++ MGX-seqstorage/src/main/java/de/cebitec/mgx/seqstorage/internal/FileUtil.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package de.cebitec.mgx.seqstorage;
+package de.cebitec.mgx.seqstorage.internal;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -14,16 +14,14 @@ import java.io.InputStream;
  *
  * @author sj
  */
-public class GZIP {
+public final class FileUtil {
 
-//    private final static byte[] GZIP_MAGIC = new byte[]{31, -117}; // 0x8b 0x1f
-
-    private GZIP() {
+    private FileUtil() {
     }
 
     public static boolean isGzip(File file) throws IOException {
         byte[] buf = new byte[2];
-        try (InputStream in = new FileInputStream(file)) {
+        try ( InputStream in = new FileInputStream(file)) {
             if (in.read(buf) < 2) {
                 return false;
             }
@@ -31,5 +29,15 @@ public class GZIP {
 
         // check for gzip magic
         return ((buf[0] == 31) && (buf[1] == -117));
+    }
+
+    public static boolean deleteDirectory(File directoryToDelete) {
+        File[] content = directoryToDelete.listFiles();
+        if (content != null) {
+            for (File f : content) {
+                deleteDirectory(f);
+            }
+        }
+        return directoryToDelete.delete();
     }
 }
