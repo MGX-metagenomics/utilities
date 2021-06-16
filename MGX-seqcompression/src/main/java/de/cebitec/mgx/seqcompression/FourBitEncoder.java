fc18512af4a31787f52895b16f098c115134dfe2
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2021-06-16 16:11:21 +0200

    fix decoding of empty sequences

--- MGX-seqcompression/src/main/java/de/cebitec/mgx/seqcompression/FourBitEncoder.java
+++ MGX-seqcompression/src/main/java/de/cebitec/mgx/seqcompression/FourBitEncoder.java
@@ -92,9 +92,10 @@ public class FourBitEncoder {
 
         int dec_len = enc.length * 2;
         // decoded sequence length is odd
-        if ((enc[enc.length - 1] & 0x0F) == 0x0) {
+        if (dec_len != 0 && (enc[enc.length - 1] & 0x0F) == 0x0) {
             dec_len--;
         }
+
         byte[] decoded = new byte[dec_len];
 
         for (int dec_pos = 0; dec_pos < dec_len; dec_pos++) {
---
+++ MGX-seqcompression/src/test/java/de/cebitec/mgx/seqcompression/FourBitEncoderTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cebitec.mgx.seqcompression;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author sj
+ */
+public class FourBitEncoderTest {
+    
+    @Test
+    public void testDecodeEmpty() {
+        System.out.println("decodeEmpty");
+        byte[] result = FourBitEncoder.decode(new byte[]{});
+        assertEquals(0, result.length);
+    }
+
+    
+}
