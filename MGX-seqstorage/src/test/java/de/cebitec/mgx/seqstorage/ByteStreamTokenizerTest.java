7281f9d5d9b75353e1d6e62405ebaf9320d3468e
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2022-11-09 13:56:57 +0100

    rm pax exam from seqstorage

--- MGX-seqstorage/src/test/java/de/cebitec/mgx/seqstorage/ByteStreamTokenizerTest.java
+++ MGX-seqstorage/src/test/java/de/cebitec/mgx/seqstorage/ByteStreamTokenizerTest.java
@@ -5,50 +5,39 @@
  */
 package de.cebitec.mgx.seqstorage;
 
-import de.cebitec.mgx.osgiutils.MGXOptions;
 import de.cebitec.mgx.testutils.TestInput;
 import java.io.File;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.runner.RunWith;
-import org.ops4j.pax.exam.Configuration;
-import static org.ops4j.pax.exam.CoreOptions.bundle;
-import static org.ops4j.pax.exam.CoreOptions.junitBundles;
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.CoreOptions.options;
-import static org.ops4j.pax.exam.CoreOptions.systemProperty;
-import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.junit.PaxExam;
 
 /**
  *
  * @author sjaenick
  */
-@RunWith(PaxExam.class)
 public class ByteStreamTokenizerTest {
 
-    @Configuration
-    public static Option[] configuration() {
-        return options(
-                junitBundles(),
-                mavenBundle().groupId("de.cebitec.mgx").artifactId("MGX-isequences").versionAsInProject(),
-                mavenBundle().groupId("de.cebitec.mgx").artifactId("MGX-seqcompression").versionAsInProject(),
-                mavenBundle().groupId("de.cebitec.mgx").artifactId("SFFReader").versionAsInProject(),
-                mavenBundle().groupId("de.cebitec.mgx").artifactId("MGX-BufferedRandomAccessFile").versionAsInProject(),
-                mavenBundle().groupId("de.cebitec.mgx").artifactId("Trove-OSGi").versionAsInProject(),
-                mavenBundle().groupId("org.apache.commons").artifactId("commons-math3").versionAsInProject(),
-                //MGXOptions.serviceLoaderBundles(),
-                MGXOptions.testUtils(),
-                systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("WARN"),
-                bundle("reference:file:target/classes")
-        );
-    }
+//    @Configuration
+//    public static Option[] configuration() {
+//        return options(
+//                junitBundles(),
+//                mavenBundle().groupId("de.cebitec.mgx").artifactId("MGX-isequences").versionAsInProject(),
+//                mavenBundle().groupId("de.cebitec.mgx").artifactId("MGX-seqcompression").versionAsInProject(),
+//                mavenBundle().groupId("de.cebitec.mgx").artifactId("SFFReader").versionAsInProject(),
+//                mavenBundle().groupId("de.cebitec.mgx").artifactId("MGX-BufferedRandomAccessFile").versionAsInProject(),
+//                mavenBundle().groupId("de.cebitec.mgx").artifactId("Trove-OSGi").versionAsInProject(),
+//                mavenBundle().groupId("org.apache.commons").artifactId("commons-math3").versionAsInProject(),
+//                //MGXOptions.serviceLoaderBundles(),
+//                MGXOptions.testUtils(),
+//                systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("WARN"),
+//                bundle("reference:file:target/classes")
+//        );
+//    }
 
     @Test
     public void testNoLineBreak() throws Exception {
         System.out.println("testNoLineBreak");
         // file has no line break after last line
-        File f = TestInput.copyTestResource(getClass(), "/de/cebitec/mgx/seqstorage/incomplete_last_line.fq");
+        File f = TestInput.copyTestResource(getClass(), "de/cebitec/mgx/seqstorage/incomplete_last_line.fq");
         byte LINEBREAK = '\n';
         ByteStreamTokenizer bst = new ByteStreamTokenizer(f.getAbsolutePath(), false, LINEBREAK, 0);
         int cnt = 0;
--- MGX-seqstorage/src/test/java/de/cebitec/mgx/seqstorage/CSFWriterTest.java
+++ MGX-seqstorage/src/test/java/de/cebitec/mgx/seqstorage/CSFWriterTest.java
@@ -5,7 +5,6 @@
  */
 package de.cebitec.mgx.seqstorage;
 
-import de.cebitec.mgx.osgiutils.MGXOptions;
 import de.cebitec.mgx.sequence.DNAQualitySequenceI;
 import de.cebitec.mgx.sequence.DNASequenceI;
 import de.cebitec.mgx.sequence.SeqStoreException;
@@ -13,44 +12,34 @@ import de.cebitec.mgx.testutils.TestInput;
 import java.io.File;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.runner.RunWith;
-import org.ops4j.pax.exam.Configuration;
-import static org.ops4j.pax.exam.CoreOptions.bundle;
-import static org.ops4j.pax.exam.CoreOptions.junitBundles;
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.CoreOptions.options;
-import static org.ops4j.pax.exam.CoreOptions.systemProperty;
-import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.junit.PaxExam;
 
 /**
  *
  * @author sj
  */
-@RunWith(PaxExam.class)
 public class CSFWriterTest {
 
-    @Configuration
-    public static Option[] configuration() {
-        return options(
-                junitBundles(),
-                mavenBundle().groupId("de.cebitec.mgx").artifactId("MGX-isequences").versionAsInProject(),
-                mavenBundle().groupId("de.cebitec.mgx").artifactId("MGX-seqcompression").versionAsInProject(),
-                mavenBundle().groupId("de.cebitec.mgx").artifactId("Trove-OSGi").versionAsInProject(),
-                mavenBundle().groupId("de.cebitec.mgx").artifactId("MGX-BufferedRandomAccessFile").versionAsInProject(),
-                mavenBundle().groupId("de.cebitec.mgx").artifactId("SFFReader").versionAsInProject(),
-                mavenBundle().groupId("org.apache.commons").artifactId("commons-math3").versionAsInProject(),
-                //MGXOptions.serviceLoaderBundles(),
-                MGXOptions.testUtils(),
-                systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("WARN"),
-                bundle("reference:file:target/classes")
-        );
-    }
+//    @Configuration
+//    public static Option[] configuration() {
+//        return options(
+//                junitBundles(),
+//                mavenBundle().groupId("de.cebitec.mgx").artifactId("MGX-isequences").versionAsInProject(),
+//                mavenBundle().groupId("de.cebitec.mgx").artifactId("MGX-seqcompression").versionAsInProject(),
+//                mavenBundle().groupId("de.cebitec.mgx").artifactId("Trove-OSGi").versionAsInProject(),
+//                mavenBundle().groupId("de.cebitec.mgx").artifactId("MGX-BufferedRandomAccessFile").versionAsInProject(),
+//                mavenBundle().groupId("de.cebitec.mgx").artifactId("SFFReader").versionAsInProject(),
+//                mavenBundle().groupId("org.apache.commons").artifactId("commons-math3").versionAsInProject(),
+//                //MGXOptions.serviceLoaderBundles(),
+//                MGXOptions.testUtils(),
+//                systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("WARN"),
+//                bundle("reference:file:target/classes")
+//        );
+//    }
 
     @Test
     public void testCSFWriter() throws Exception {
         System.out.println("testCSFWriter");
-        File f = TestInput.copyTestResource(getClass(), "/de/cebitec/mgx/seqstorage/test.fas");
+        File f = TestInput.copyTestResource(getClass(), "de/cebitec/mgx/seqstorage/test.fas");
         File target = File.createTempFile("testCSFWriter", "xx");
         target.delete();
         try (FastaReader fr = new FastaReader(f.getAbsolutePath(), false)) {
@@ -77,7 +66,7 @@ public class CSFWriterTest {
     @Test
     public void testCSFReader() throws Exception {
         System.out.println("testCSFReader");
-        File f = TestInput.copyTestResource(getClass(), "/de/cebitec/mgx/seqstorage/test.fas");
+        File f = TestInput.copyTestResource(getClass(), "de/cebitec/mgx/seqstorage/test.fas");
         File target = File.createTempFile("testCSFReader", "xx");
         target.delete();
         try (FastaReader fr = new FastaReader(f.getAbsolutePath(), false)) {
@@ -112,7 +101,7 @@ public class CSFWriterTest {
     @Test
     public void testLongRead() throws Exception {
         System.out.println("testLongRead");
-        File f = TestInput.copyTestResource(getClass(), "/de/cebitec/mgx/seqstorage/nptest.fq");
+        File f = TestInput.copyTestResource(getClass(), "de/cebitec/mgx/seqstorage/nptest.fq");
         File target = File.createTempFile("testCSFReader", "xxx");
         target.delete();
 
