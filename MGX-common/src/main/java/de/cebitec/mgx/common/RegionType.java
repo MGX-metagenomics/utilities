6ab22f4c70da65bb02a2bd57076d9bfc6e2a6927
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2023-01-04 15:19:25 +0100

    add region type names for display

--- MGX-common/src/main/java/de/cebitec/mgx/common/RegionType.java
+++ MGX-common/src/main/java/de/cebitec/mgx/common/RegionType.java
@@ -6,21 +6,28 @@ package de.cebitec.mgx.common;
  */
 public enum RegionType {
 
-    CDS(0),
-    RRNA(1),
-    TRNA(2),
-    TMRNA(3),
-    NCRNA(4),
-    MISC(5);
+    CDS(0, "CDS"),
+    RRNA(1, "rRNA"),
+    TRNA(2, "tRNA"),
+    TMRNA(3, "tmRNA"),
+    NCRNA(4, "ncRNA"),
+    MISC(5, "misc");
     
 
     private final int code;
+    private final String s;
 
-    private RegionType(int c) {
+    private RegionType(int c, String text) {
         code = c;
+        s = text;
     }
 
     public int getValue() {
         return code;
+    }
+
+    @Override
+    public String toString() {
+        return s;
     }
 }
